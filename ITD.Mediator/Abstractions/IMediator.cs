c048b7841dead3b06535850209e1abf0becbf006
Tomasz Ignaszewski <tomasz.ignaszewski@also.com>
2025-04-10 12:47:38 +0200

    initial create

--- ITD.Mediator/Abstractions/IMediator.cs
+++ ITD.Mediator/Abstractions/IMediator.cs
@@ -2,5 +2,6 @@
 public interface IMediator
 {
     Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
-    Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken = default);
+    Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken = default)
+        where TNotification : INotification;
 }
